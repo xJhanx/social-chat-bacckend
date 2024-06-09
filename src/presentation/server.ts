cb0ffdf5301ea3f1abf1a0946b73b161248a4b98
xJhanx <lopezyarce.jhancarlos@gmail.com>
2024-06-09 13:17:43 -0500

    refractor : simplementation singlenton for sockets

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,23 +1,39 @@
 import express from 'express';
 import { Routes } from './routes/router';
 import { AuthMiddleware } from './middlewares';
-export class Server {
+import http from 'http';
+import { SocketIoServer } from './socket';
+import cors from 'cors';
+
+export class App {
 
     private readonly app = express();
-    constructor(public port: number) {}
+    constructor(public port: number) { }
 
     public start = () => {
         /**CORS */
+        this.app.use(cors({
+            origin: ["http://empresatest:4200"], // <--- Agrega tu origen permitido aquí
+            methods: ["GET", "POST"],
+            allowedHeaders: ["Authorization"],
+            credentials: true,
+            
+        }));
         // set the cors policy
-        
+
         /**Midlewares */
-        this.app.use(express.json()); 
+        this.app.use(express.json());
         this.app.use(express.urlencoded({ extended: true }));
-        this.app.use(AuthMiddleware.auth);
+        // this.app.use(AuthMiddleware.auth);
         /**Routes */
         this.app.use(Routes.routes);
-        this.app.listen(this.port, () => {
+
+        /**Initialize server  */
+
+        const httpServer = http.createServer(this.app);
+        const socket = SocketIoServer.initialize(httpServer).io;
+        httpServer.listen(this.port, () => {
             console.log(`Server running on port ${this.port}`)
-        })
+        });
     }
 }
